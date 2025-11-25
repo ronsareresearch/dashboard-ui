230ebd16b5ecf87fee53ddaefaf84738883a7911
sanjayachari <sanjayacharya992@gmail.com>
2025-11-25 20:38:54 +0530

    fix: Update API_BASE_URL to use production endpoint

--- app/constant/constant.js
+++ app/constant/constant.js
@@ -1,3 +1,3 @@
-// export const API_BASE_URL = "https://chatapi.ronsare.site";
-export const API_BASE_URL = "http://localhost:4000";
+export const API_BASE_URL = "https://chatapi.ronsare.site";
+// export const API_BASE_URL = "http://localhost:4000";
 export const WS_URL = "wss://chatapi.ronsare.site/messages/ws";
